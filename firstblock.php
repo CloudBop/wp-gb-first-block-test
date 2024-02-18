ff525661aa08831e9a5e6af573b1a978270b9733
Cloudbop <boppincolin@gmail.com>
2024-02-18 15:07:42 +0000

    cmnt for WP plugin registration

---
+++ firstblock.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: First Block
+ * Plugin URI: https://colinrowntree.com/
+ * Description: My first block
+ * Author: 0xColinR
+ * Author URI: https://colinrowntree.com/
+ */
+
+ 
