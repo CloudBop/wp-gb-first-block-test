2b2f15fdef5372dac4d3b4ef98c51200c97d5ecf
Cloudbop <boppincolin@gmail.com>
2024-02-18 15:51:21 +0000

    +updated block edit/save

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+// isn't actually included in code bundle as it is already enqued via wordpress/code
 // var registerBlockType = wp.blocks.registerBlockType;
 import { registerBlockType } from '@wordpress/blocks';
 
 registerBlockType('blocks-course/firstblock', {
   edit: function () {
-    return <p className="class">Edit JSX</p>;
+    return <p className="class">Edit the JSX</p>;
   },
   save: function () {
-    return <p className="class">Save JSX</p>;
+    return <p className="class">Save the JSX</p>;
   },
 });
