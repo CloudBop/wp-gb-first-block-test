1a3cbef63f80fb993603e1eaa3b689248e8334ed
Cloudbop <boppincolin@gmail.com>
2024-02-18 15:23:49 +0000

    Register new block.json

--- firstblock.php
+++ firstblock.php
@@ -8,4 +8,9 @@
  * Author URI: https://colinrowntree.com/
  */
 
- 
+function blocks_course_firstblock_init() {
+    // get the block.json
+    register_block_type_from_metadata( __DIR__ );
+}
+// initialise new block
+add_action( "init", "blocks_course_firstblock_init" );
